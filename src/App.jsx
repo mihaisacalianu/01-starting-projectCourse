1b44746cb40d7d14638a892d355449144b207e2b
mihaisacalianu <mihaisacalianu@gmail.com>
2025-04-21 21:14:19 +0100

    built first component with basic appearance

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import SideBar from "./components/SideBar";
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+     <SideBar/>
     </>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+export default function SideBar(){
+  return(
+    <>
+      <div className="flex-col justify-center items-center text-center bg-black text-white rounded-tr-lg h-[100dvh] mt-6 w-[30%]">
+        <h1 className="mb-5 mt-10">YOUR PROJECTS</h1>
+        <button className="bg-slate-400 rounded-md p-1">+ Add Project</button>
+      </div>
+    </>
+  );
+
+}
