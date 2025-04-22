b74dfd4187a907a79684d3d809a20ba0a93d790b
mihaisacalianu <mihaisacalianu@gmail.com>
2025-04-22 20:26:35 +0100

    built state for rendering new project form when button is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import SideBar from "./components/SideBar";
 import MainContent from "./components/MainContent"
 import NewProjectForm from "./components/NewProjectForm";
+import { useState } from "react";
 function App() {
+  const [newProjectForm, setNewProjectForm] = useState(false);
+  const [mainContent, setMainContent] = useState("");
+  function handleClick() {
+    setNewProjectForm(true);
+    setMainContent("hidden");
+  }
   return (
     <>
     <div className="flex">
      <SideBar/>
-     <MainContent />
-     <NewProjectForm/>
+     <MainContent
+     handleClick={handleClick}
+     hideMainContent={mainContent}
+     />
+      {newProjectForm && <NewProjectForm/>}
      </div>
     </>
   );
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,12 +1,12 @@
 import noProjects from "../assets/no-projects.png";
-export default function MainContent(){
+export default function MainContent({handleClick, hideMainContent}){
   return(
     <>
-      <div className="flex-col hidden justify-center items-center text-center h-[100dvh] mt-6 w-[70%]">
+      <div className={`flex-col ${hideMainContent} justify-center items-center text-center h-[100dvh] mt-6 w-[70%]`}>
         <img src={noProjects} alt="no projects image" className="h-[100px] w-[100px] mx-auto"/>
         <h2 className="font-extrabold mb-4">No Project Selected</h2>
         <p>Select a project or get started with a new one</p>
-        <button className="bg-black text-gray-400 px-2 py-1 rounded-lg mt-4">Create new project</button>
+        <button onClick={handleClick} className="bg-black text-gray-400 px-2 py-1 rounded-lg mt-4">Create new project</button>
       </div>
     </>
   );
